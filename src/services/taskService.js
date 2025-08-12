1a3b9bb71d910025e8f77a5b8aa5793113c0b12c
viniciuspaschoal <viniciuspaschoal1847@gmail.com>
2025-08-12 07:49:58 -0300

    Receives task in create to send to backend

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,6 +1,6 @@
 import api from './api'
 
 export const getTasks = () => api.get('/task/user/1')
-export const createTask = () => api.post('/task', task)
+export const createTask = (task) => api.post('/task', task)
 export const updateTask = task => api.put(`/task/${task.id}`, task);
 export const deleteTask = id => api.delete(`/task/${id}`);
